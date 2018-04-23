3190a4fa3f3b9614c203ffddd82db7fc04eacf24
Noah Callahan <noahc1991@gmail.com>
2018-04-23 12:26:45 -0400

    new ship form, table

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,25 +1,42 @@
 class ShipsController < ApplicationController
   def index
+    @ships = Ship.all
   end
 
   def show
   end
 
   def create
+    Ship.create(ship_params)
+    redirect_to(ships_path)
   end
 
   def new
+    @ships = Ship.all
+    @ship = Ship.new
+    
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def delete
+
   end
 
   def destroy
+
   end
+
+  private
+
+  def ship_params
+    params.require(:ship).permit(:name, :container_max, :payload, :location, :shipper_id)
+  end
+
 end
